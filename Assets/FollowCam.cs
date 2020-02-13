10547e74b047c98c0efea87524c71d8bb910f0ac
woojink3r <57843107+woojink3r@users.noreply.github.com>
2020-02-13 16:43:59 -0500

    Camera and Physical mat

---
+++ Assets/FollowCam.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    static public GameObject POI;
+
+    [Header("Set Dynamically")]
+    public float camZ;
+
+    void Awake()
+    {
+        camZ = this.transform.position.z;
+    }
+
+    void FixedUpdate()
+    {
+        if (POI == null) return;
+
+        Vector3 destination = POI.transform.position;
+        destination.z = camZ;
+        transform.position = destination;
+    }
+}
