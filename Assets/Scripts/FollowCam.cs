3314f64fcb22f29d59be4035c118d4e87929b712
woojink3r <57843107+woojink3r@users.noreply.github.com>
2020-02-16 17:25:12 -0500

    Projectile Line

--- Assets/Scripts/FollowCam.cs
+++ Assets/Scripts/FollowCam.cs
@@ -18,11 +18,27 @@ public class FollowCam : MonoBehaviour
         camZ = this.transform.position.z;
     }
 
+    Vector3 destination;
     void FixedUpdate()
     {
-        if (POI == null) return;
+        if (POI == null)
+        {
+            destination = Vector3.zero;
+        }
+        else 
+        {
+            destination = POI.transform.position;
+            if(POI.tag == "Projectile")
+            {
+                if (POI.GetComponent<Rigidbody>().IsSleeping())
+                {
+                    POI = null;
+                    return;
+                }
+            }
+        }
 
-        Vector3 destination = POI.transform.position;
+        //Vector3 destination = POI.transform.position;
         destination.x = Mathf.Max(minXY.x, destination.x);
         destination.y = Mathf.Max(minXY.y, destination.y);
         destination = Vector3.Lerp(transform.position, destination, easing);
